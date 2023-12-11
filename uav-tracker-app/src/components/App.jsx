a1d20fcba5cb4c22e44b40e28bfd346ecee7001f
PloKoon68 <ors_mehmet68@hotmail.com>
2023-12-11 22:15:40 +0300

    Added the navigation bars.

--- uav-tracker-app/src/components/App.jsx
+++ uav-tracker-app/src/components/App.jsx
@@ -10,19 +10,39 @@ import EnterUav from './navigate/EnterUav.jsx'
 
 function App() {
   const [dataState, setDataState] = useState(null);
-  //console.log("Achieved new Data: ", dataState)
+  const [currentSection, setCurrentSection] = useState('ucaklar');
 
 
   function updateDataState(newData)  {
     //console.log(newData)
     setDataState(newData);
   }
+
+  const handleNavClick = (section) => {
+    setCurrentSection(section);
+  };
+
   return (
     <>
+      <nav className="navbar">
+        <button className="navBars" onClick={() => handleNavClick('ucaklar')}>Uçaklar</button>
+        <button className="navBars" onClick={() => handleNavClick('otherSection1')}>Other Section 1</button>
+        <button className="navBars" onClick={() => handleNavClick('otherSection2')}>Other Section 2</button>
+        <button className="navBars" onClick={() => handleNavClick('otherSection3')}>Other Section 3</button>
+      </nav>
+
       <header className="App-header">
+      {currentSection === 'ucaklar' && (
+          <>
+            <DynamicTable newData={dataState} />
+            <EnterUav newRow={updateDataState} />
+          </>
+        )}
+        {currentSection === 'otherSection1' && <p>Content for Other Section 1</p>}
+        {currentSection === 'otherSection2' && <p>Content for Other Section 2</p>}
+        {currentSection === 'otherSection3' && <p>Content for Other Section 3</p>}
+      
       </header>
-      <DynamicTable newData={dataState}/>
-      <EnterUav  newRow={updateDataState}/>
     </>
   );
 }
